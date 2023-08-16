713b627880d1f7559695ebe55d4e1481b589aa08
zzt244 <pu.j8.60.230@gmail.com>
2023-08-16 22:01:53 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :nickname, :birthday, presence: true
     validates :familyname, :firstname, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :familyname_kana, :firstname_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :password, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: {with: VALID_PASSWORD_REGEX, message: "is invalid. Input half-width characters."}
   end
 end
