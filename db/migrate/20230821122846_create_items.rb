ff9764d4b2f22e0581b745889d87fb098553f022
zzt244 <pu.j8.60.230@gmail.com>
2023-08-21 22:03:46 +0900

    itemsテーブルの作成

---
+++ db/migrate/20230821122846_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :itemname,         null: false
+      t.text       :explanation,      null: false
+      t.integer    :category_id,      null: false
+      t.integer    :status_id,        null: false
+      t.integer    :shipping_cost_id, null: false
+      t.integer    :prefecture_id,    null: false
+      t.integer    :shipping_date_id, null: false
+      t.integer    :prise,            null: false
+      t.references :user,             null: false, foreign_key: true
+      
+      t.timestamps
+    end
+  end
+end
