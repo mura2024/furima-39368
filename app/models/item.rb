249d8fa1acb418723c3bf23b9cef66e64ac2b919
zzt244 <pu.j8.60.230@gmail.com>
2023-08-25 23:27:47 +0900

    active hashのモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
-  
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :itemname
+    validates :explanation
+    validates :prise, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+              format: { with: /\A[0-9]+\z/ }
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
 end
