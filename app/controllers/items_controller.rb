2d287a7f61466de76d01b042107d9e30d0121ae3
zzt244 <pu.j8.60.230@gmail.com>
2023-09-09 18:57:56 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,11 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      render :show
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
