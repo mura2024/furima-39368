239f92407edc379ed5c08eacf5aa35d25f76e0f4
zzt244 <pu.j8.60.230@gmail.com>
2023-09-20 23:34:54 +0900

    order_addressモデルのテスト

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code     { '123-4567' }
+    prefecture_id { 3 }
+    city          { Faker::Address.city }
+    house_number  { Faker::Address.street_address }
+    building      { Faker::Lorem.sentence }
+    phone_number  { '09012341234' }
+  end
+end
