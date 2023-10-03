13d8a9f65b8237a3f9d4806354616ade3d58b9fc
zzt244 <pu.j8.60.230@gmail.com>
2023-10-03 23:27:41 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  with_options numericality: { other_than: 1, message: "can't be blank" } do
+  with_options numericality: { other_than: 1, message: "を選択してください。" } do
     validates :category_id
     validates :status_id
     validates :shipping_cost_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,11 +17,11 @@ class User < ApplicationRecord
   end
 
   with_options allow_blank: true do
-    validates :familyname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :firstname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
-    validates :familyname_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :firstname_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :familyname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は無効です。全角文字を入力してください。' }
+    validates :firstname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は無効です。全角文字を入力してください。' }
+    validates :familyname_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'は無効です。全角カタカナを入力してください。' }
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'は無効です。全角カタカナを入力してください。' }
     validates :password,
-              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input half-width characters.' }
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は無効です。半角文字を入力してください。' }
   end
 end
