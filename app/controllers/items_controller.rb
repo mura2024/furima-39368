1e481f8e5aec4356e6858ba33284f23477ebaf30
zzt244 <pu.j8.60.230@gmail.com>
2023-10-13 20:27:26 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :search_product, only: [:index, :show, :search]
@@ -61,5 +61,4 @@ class ItemsController < ApplicationController
     @q = Item.ransack(params[:q])
     @results = @q.result
   end
-
 end
